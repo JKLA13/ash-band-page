a2fb154bea08e9f3de4cbb33ed65dbf7784bff78
Jacob Adamson <jakeladamson0@gmail.com>
2021-11-02 21:54:40 -0600

    navbar working

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { setContext } from "@apollo/client/link/context";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+    <>
+      <NavBar />
+    </>
+    // </Router>
   );
 }
 
