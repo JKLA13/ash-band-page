02f86d67a4af21c80a51fcd78ce3e4f8fb83fe33
Jacob Adamson <jakeladamson0@gmail.com>
2021-11-03 20:27:12 -0600

    setup page files, custom font buggy

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/fonts/stylesheet.css";
 // import { setContext } from "@apollo/client/link/context";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import { BrowserRouter as Router } from "react-router-dom";
