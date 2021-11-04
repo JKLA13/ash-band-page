6f07060c5b30c3b20d8a0e4bc7461b5ebc07ab9a
Jacob Adamson <jakeladamson0@gmail.com>
2021-11-03 21:35:59 -0600

    css styles buggy, home still working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,14 +4,15 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Picture from "../assets/images/IMG_0021.JPG";
+import "../fonts/stylesheet.css";
 
 function Home() {
   return (
     <>
-      <Container fluid bg="dark" variant="dark">
+      <Container fluid>
         <Row>
           <Col>
-            <h1>Album Coming Soon</h1>
+            <h2 className="align-content-center">***Album Coming Soon***</h2>
             <Image src={Picture} className="me-3 img-fluid" />
           </Col>
         </Row>
