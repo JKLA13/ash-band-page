b8d17150ab12bef74d17a5454b2c3728943dd8f8
Jacob Adamson <jakeladamson0@gmail.com>
2021-11-03 23:13:17 -0600

    router buggy, page functional still

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Music from "./pages/Music";
+import Media from "./pages/Media";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import "../src/fonts/stylesheet.css";
 import "../src/App.css";
 // import { setContext } from "@apollo/client/link/context";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    // <Router>
     <>
-      <NavBar />
-      <Home />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/Music">
+            <Music />
+          </Route>
+          <Route exact path="/Media">
+            <Media />
+          </Route>
+        </Switch>
+      </Router>
     </>
-    // </Router>
   );
 }
 
