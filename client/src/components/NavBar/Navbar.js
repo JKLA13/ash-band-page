40387ecacecb9a69990a56c60091a10d1d061ec6
Jacob Adamson <jakeladamson0@gmail.com>
2021-11-08 19:21:51 -0700

    added to README

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -2,27 +2,44 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import Picture from "../../assets/images/IMG_0026.PNG";
+import "../../fonts/stylesheet.css";
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar
+        sticky="top"
+        collapseOnSelect
+        expand="lg"
+        bg="black"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand>
+            <Image src={Picture} className="me-1 img-fluid" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#Home" to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link href="#About" to="/About">
+                About
+              </Nav.Link>
+              <Nav.Link href="#Music" to="/Music">
+                Music
+              </Nav.Link>
+              <Nav.Link href="#Media" to="/Media">
+                Media
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
