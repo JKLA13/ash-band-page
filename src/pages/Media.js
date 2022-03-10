8ad3a237f7d4e05df39408624d0a77d90f6ccd51
Jacob Adamson <jakeladamson0@gmail.com>
2022-03-09 22:58:59 -0700

    worked on text sizes in media js

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -16,7 +16,6 @@ import Picture7 from "../assets/images/IMG_9023.jpg";
 import Picture8 from "../assets/images/IMG_9034.jpg";
 import Picture9 from "../assets/images/IMG_7457.jpg";
 // import Picture10 from "../assets/images/IMG_7274.jpg";
-
 import "../fonts/stylesheet.css";
 
 function Media() {
@@ -29,67 +28,67 @@ function Media() {
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture00} alt="00" />
                 <Carousel.Caption>
-                  <p>Hold steady.</p>
+                  <h2>Hold steady.</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture0} alt="0" />
                 <Carousel.Caption>
-                  <p>Live at Skylark.</p>
+                  <h2>Live at Skylark.</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture1} alt="1" />
                 <Carousel.Caption>
-                  <p>Recording the spectre.</p>
+                  <h2>Recording the spectre.</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture2} alt="2" />
                 <Carousel.Caption>
-                  <p>Rehearsing the cover song.</p>
+                  <h2>Rehearsing the cover song.</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture3} alt="3" />
                 <Carousel.Caption>
-                  <p>Laying down voices...</p>
+                  <h2>Laying down voices...</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture4} alt="4" />
                 <Carousel.Caption>
-                  <p>Laying down more voices...</p>
+                  <h2>Laying down more voices...</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture5} alt="5" />
                 <Carousel.Caption>
-                  <p>Laying down even more voices...</p>
+                  <h2>Laying down even more voices...</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture6} alt="6" />
                 <Carousel.Caption>
-                  <p>Mixing the noises.</p>
+                  <h2>Mixing the noises.</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture9} alt="10" />
                 <Carousel.Caption>
-                  <p>Matt playing the... guitar?</p>
+                  <h2>Matt playing the... guitar?</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture7} alt="7" />
                 <Carousel.Caption>
-                  <p>Wirey, werewolf and bass playing.</p>
+                  <h2>Wirey, werewolf and bass playing.</h2>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100 photo" src={Picture8} alt="8" />
                 <Carousel.Caption>
-                  <p>Synths + whiskey.</p>
+                  <h2>Synths + whiskey.</h2>
                 </Carousel.Caption>
               </Carousel.Item>
             </Carousel>
